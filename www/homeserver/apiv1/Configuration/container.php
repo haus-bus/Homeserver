ab9a1bc10c66b34e43d3041a5b9d68c5ba8f1587
Viktor Pankraz <info@pankraz.org>
2018-08-19 01:25:22 +0200

    fix for apiv1

--- www/homeserver/apiv1/Configuration/container.php
+++ www/homeserver/apiv1/Configuration/container.php
@@ -9,9 +9,9 @@ $containerBuilder->useAnnotations(true);
 $containerBuilder->addDefinitions(__DIR__ . '/config.php');
 $containerBuilder->addDefinitions(__DIR__ . '/config.prod.php');
 
-$containerBuilder->setDefinitionCache(new Doctrine\Common\Cache\ApcuCache());
+//$containerBuilder->setDefinitionCache(new Doctrine\Common\Cache\ApcuCache());
 // todo: activate arrayCache while development
-//$containerBuilder->setDefinitionCache(new Doctrine\Common\Cache\ArrayCache());
+$containerBuilder->setDefinitionCache(new Doctrine\Common\Cache\ArrayCache());
 $container = $containerBuilder->build();
 
 return $container;
